f1ce59dc42673d6574fffa696997e5c1256760f3
unknown <ortiztato@gmail.com>
2022-06-27 16:33:59 -0600

    arreglado el nav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
+const aboutbtn = document.querySelector("#aboutbtn");
+const skillsbtn = document.querySelector("#skillsbtn");
+const workbtn = document.querySelector("#workbtn");
+const contactbtn = document.querySelector("#contactbtn");
 
 function toggleMenu() {
   menu.classList.toggle("menu_opened");
@@ -8,4 +12,7 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
-menu.addEventListener("click", toggleMenu);
+aboutbtn.addEventListener("click", toggleMenu);
+skillsbtn.addEventListener("click", toggleMenu);
+workbtn.addEventListener("click", toggleMenu);
+contactbtn.addEventListener("click", toggleMenu);
